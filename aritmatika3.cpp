f5e209780ed9a6b1628b6f610eff8d43be643f30
Erlangga Dwi Jiwantoro <83545747+erlanggadj@users.noreply.github.com>
2021-07-11 22:24:58 +0700

    Update aritmatika3.cpp

--- aritmatika3.cpp
+++ aritmatika3.cpp
@@ -58,6 +58,103 @@ bool precedence (string a, string b){
 	return value1 <= value2;
 }
 
+void inputInfix(){
+	
+	//menyimpan inputan infix tanpa spasi
+	while (cin.get(input)){
+		if (input != ' '){
+			simpan.push_back(input);
+		}
+		if (input == '\n'){
+			break;
+		}
+	}
+	
+	for (int i = 0; i < (int) simpan.size(); i++){
+		
+		if (isOperand(simpan[i]) || (simpan[i] == '-' && i == 0 && isOperand(simpan[i+1]))){
+			temp.push_back(simpan[i]);
+			continue;
+		}
+		if (isOperator(simpan[i])){
+			if ((int) temp.size() != 0){
+				infix.push_back(temp);
+				temp.clear();
+			}
+			if ((simpan[i] == '-' && isOperator(simpan[i-1]))
+			|| (simpan[i] == '-' && i == 0 && (simpan[i+1] == '('))
+			|| (simpan[i] == '-' && (simpan[i-1] == '('))){
+				infix.push_back("-1");
+				infix.push_back("*");
+			}
+			else {
+				infix.push_back(simpan.substr(i, 1));
+			}
+		}
+		if (isParenthesis(simpan[i])){
+			if ((int) temp.size() != 0){
+				infix.push_back(temp);
+				temp.clear();
+			}
+			infix.push_back(simpan.substr(i, 1));
+		}
+	}
+	
+	if ((int) temp.size() != 0){
+		infix.push_back(temp);
+		temp.clear();
+	}
+	
+	//for (auto x = infix.begin(); x != infix.end(); ++x){
+	//	cout << *x << " ";
+	//}
+}
+
+vector <string>::iterator j;
+
+void toPostfix(){
+	int y = 0;
+	for (j = infix.begin(); j != infix.end(); j++, y++){
+		if (isdigit(infix[y].back())){
+			postfix.push_back(infix[y]);
+			continue;
+		}
+		if (infix[y] == "("){
+			P.push(infix[y]);
+			continue;
+		}
+		if (infix[y] == ")"){
+			while (!P.empty() && (P.top() != "(")){
+				add = P.top();
+				postfix.push_back(add);
+				P.pop();
+			}
+			P.pop();
+			continue;
+		}
+		if (isOperator(infix[y][0])){
+			if (P.empty() || (P.top() == "(")){
+				P.push(infix[y]);
+			}
+			else {
+				while (!P.empty() && (P.top() != "(") && precedence(infix[y], P.top())){
+					add = P.top();
+					postfix.push_back(add);
+					P.pop();
+				}
+				P.push(infix[y]);
+			}
+			continue;
+		}
+	}
+	
+	while (!P.empty()){
+		add = P.top();
+		postfix.push_back(add);
+		P.pop();
+	}
+}
+
 int main(){
   
 }
